0f39c899b03d96468ee4bca839178d1724a9726b
rezoaan <141851158+rezoaan@users.noreply.github.com>
2023-10-31 17:38:37 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-var pData = Number(prompt('Please enter the year of your birth'));
-var age = 2023 - pData
-if (age > 18) {
-  document.write(`<h1 style="color: green;"> Congratulations! Your can apply for your NID card. You are ${age} years old </h1>`);
-} else if ( age == 18) {
-  document.write(`<h1 style="color: blue;"> Congratulations! Your can apply for your NID card But to be received it next year. You are ${age} years old </h1>`);
-}
+$(document).ready(function () {
+  $('.hide').click(function() {
+    $('.box').hide(500);
+  })
 
-else {
-  document.write(`<h1 style="color: red;"> Sorry! You can't apply for your NID card. You are ${age} years old </h1>`);
-}
-
-
-document.querySelector('.btnActive').addEventListener('click', function(){
-  document.querySelector('.popup').style.display = "block"
-})
-
-document.querySelector('.close_btn').addEventListener('click', function(){
-  document.querySelector('.popup').style.display = "none"
-})
-
-
+  $('.show').click(function() {
+    $('.box').show(500);
+  })
+    $('.toggle').click(function() {
+      $('.box').toggle(500);
+    
+  })
+});
 
